e7cc7af88137a9279de8125913b5e41772ac71a9
KisaragiEffective <kisaragi.effective@gmail.com>
2020-06-23 22:08:36 +0900

    deprecate those method which is hard to understand what they will do

--- DynmapCore/src/main/java/org/dynmap/JSONUtils.java
+++ DynmapCore/src/main/java/org/dynmap/JSONUtils.java
@@ -3,10 +3,28 @@ package org.dynmap;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.util.Arrays;
+
 public class JSONUtils {
-    
-    // Gets a value at the specified path.
+    /**
+     * Gets a value at the specified path.
+     * @deprecated Use {@link JSONUtils#getValue(JSONObject, String)} instead.
+     * @param o the object
+     * @param path the path, joined by '/'.
+     * @return the value
+     */
+    @Deprecated
     public static Object g(JSONObject o, String path) {
+        return getValue(o, path);
+    }
+
+    /**
+     * Gets a value at the specified path.
+     * @param o the object
+     * @param path the path, joined by '/'.
+     * @return the value
+     */
+    public static Object getValue(JSONObject o, String path) {
         int index = path.indexOf('/');
         if (index == -1) {
             return o.get(path);
@@ -20,13 +38,20 @@ public class JSONUtils {
             } else /*if (oo instanceof JSONObject)*/ {
                 subobject = o;
             }
-            return g(subobject, subpath);
+            return getValue(subobject, subpath);
         }
     }
-    
-    // Sets a value on the specified path. If JSONObjects inside the path are missing, they'll be created.
-    @SuppressWarnings("unchecked")
+    /**
+     * Sets a value on the specified path. If JSONObjects inside the path are missing, they'll be created.
+     * @deprecated Use {@link JSONUtils#setValue(JSONObject, String, Object)} instead.
+     */
+    @Deprecated
     public static void s(JSONObject o, String path, Object value) {
+        setValue(o, path, value);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static void setValue(JSONObject o, String path, Object value) {
         int index = path.indexOf('/');
         if (index == -1) {
             o.put(path, value);
@@ -41,32 +66,52 @@ public class JSONUtils {
             } else /*if (oo instanceof JSONObject)*/ {
                 subobject = (JSONObject)oo;
             }
-            s(subobject, subpath, value);
+            setValue(subobject, subpath, value);
         }
     }
-    
-    // Adds a value to the list at the specified path. If the list does not exist, it will be created.
-    @SuppressWarnings("unchecked")
+
+    /**
+     * Adds a value to the list at the specified path. If the list does not exist, it will be created.
+     * @deprecated Use {@link JSONUtils#array(JSONObject, String, Object)} instead.
+     */
+    @Deprecated
     public static void a(JSONObject o, String path, Object value) {
-        Object oo = g(o, path);
+        array(o, path, value);
+    }
+
+    /**
+     * Adds a value to the list at the specified path. If the list does not exist, it will be created.
+     */
+    @SuppressWarnings("unchecked")
+    public static void array(JSONObject o, String path, Object value) {
+        Object oo = getValue(o, path);
         JSONArray array;
         if (oo == null) {
-            array =new JSONArray();
-            s(o, path, array);
+            array = new JSONArray();
+            setValue(o, path, array);
         } else {
             array = (JSONArray)oo;
         }
         if(value != null)
             array.add(value);
     }
-    
-    // Simply creates a JSONArray.
-    @SuppressWarnings("unchecked")
+
+    /**
+     * Simply creates a JSONArray.
+     * @deprecated Use {@link JSONUtils#makeJSONArray(Object...)} instead.
+     */
+    @Deprecated
     public static JSONArray l(Object... items) {
+        return makeJSONArray(items);
+    }
+
+    /**
+     * Simply creates a JSONArray.
+     */
+    @SuppressWarnings("unchecked")
+    public static JSONArray makeJSONArray(Object... items) {
         JSONArray arr = new JSONArray();
-        for(Object item : items) {
-            arr.add(item);
-        }
+        arr.addAll(Arrays.asList(items));
         return arr;
     }
 }
